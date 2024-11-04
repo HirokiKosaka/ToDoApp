8e1476bc5760c70f2eb9bbeb82169fca338e6540
arp.kosaka <h.kosaka.arp@gmail.com>
2024-11-04 17:05:51 +0900

    added buttons

--- todo/src/todo/Todo.tsx
+++ todo/src/todo/Todo.tsx
@@ -14,6 +14,9 @@ const Todo = () => {
             <button>learning English</button>
           </li>
         </ul>
+        <button className="border border-white rounded-md p-1 text-white">
+          Add Todo
+        </button>
       </div>
       <div className="w-1/3 bg-yellow-300">
         <h1 className="text-center">Under Taking</h1>
@@ -22,6 +25,14 @@ const Todo = () => {
             <button>Create Todo List</button>
           </li>
         </ul>
+        <div className="flex">
+          <button className="border border-white rounded-md p-1 text-white mr-1">
+            Undo
+          </button>
+          <button className="border border-white rounded-md p-1 text-white">
+            Done
+          </button>
+        </div>
       </div>
       <div className="w-1/3 bg-blue-300">
         <h1 className="text-center">Done</h1>
@@ -33,6 +44,9 @@ const Todo = () => {
             <button>reading</button>
           </li>
         </ul>
+        <button className="border border-white rounded-md p-1 text-white">
+          Undo
+        </button>
       </div>
     </div>
   );
