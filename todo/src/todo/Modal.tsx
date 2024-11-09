09e6f7acbedcb367bec58f79f5bf5ef4b68deb60
arp.kosaka <h.kosaka.arp@gmail.com>
2024-11-09 14:45:26 +0900

    todo機能作成

--- todo/src/todo/Modal.tsx
+++ todo/src/todo/Modal.tsx
@@ -1,10 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Todos } from "./Todo";
 
 type CloseModal = {
   closeModal: () => void;
-}
+  todos?: Todos;
+  setTodos: React.Dispatch<React.SetStateAction<Todos>>;
+};
 
-const Modal: FC<CloseModal> = ({ closeModal }) => {
+const Modal: FC<CloseModal> = ({ closeModal, setTodos }) => {
+  const [text, setText] = useState<string>("");
+  const handleInput: (e: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    setText(e.target.value);
+  };
+  const addTodos: () => void = () => {
+    setTodos((prevTodos) => {
+      if (text !== "") {
+        return [...prevTodos, text];
+      }
+      return prevTodos;
+    });
+    setText("");
+  };
   return (
     <div className="flex items-center absolute top-0 left-28 w-full h-full z-50">
       <div className="flex justify-center items-center border rounded-md mt-5 bg-slate-500 shadow-2xl">
@@ -12,11 +28,16 @@ const Modal: FC<CloseModal> = ({ closeModal }) => {
           type="text"
           placeholder="Todoを追加"
           className="border flex justify-start m-3 rounded-md p-1"
+          onChange={handleInput}
+          value={text}
         />
         <button className="mr-3 rounded-md p-1" onClick={closeModal}>
           閉じる
         </button>
-        <button className="mr-3 rounded-md p-1 bg-blue-700 text-white">
+        <button
+          onClick={addTodos}
+          className="mr-3 rounded-md p-1 bg-blue-700 text-white"
+        >
           追加
         </button>
       </div>
--- todo/src/todo/Todo.tsx
+++ todo/src/todo/Todo.tsx
@@ -1,8 +1,43 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import Modal from "./Modal";
 
-const Todo = () => {
-  const [isModal, setIsModal] = useState(false);
+export type Todos = string[];
+
+const Todo: FC = () => {
+  // todosの配列
+  const [todos, setTodos] = useState<Todos>([]);
+  // takingの配列
+  const [taking, setTaking] = useState<Todos>([]);
+  // doneの配列
+  const [dones, setDones] = useState<Todos>([]);
+  // todoから指定された要素を削除する関数
+  const handleDelete: (todo: string) => void = (todo) => {
+    setTodos(todos.filter((item) => item !== todo));
+  };
+
+  // todo->takingへ移動する関数
+  const handleTaking: (todo: string) => void = (todo) => {
+    if (!taking.includes(todo)) {
+      setTaking([...taking, todo]);
+    }
+    handleDelete(todo);
+  };
+  // taking->todosへ戻す関数
+  const handleBackToTodos: (item: string) => void = (item) => {
+    setTodos([...todos, item]);
+    setTaking(taking.filter((todo) => todo !== item));
+  };
+  // taking->doneへ移動する関数
+  const handleDone: (take: string) => void = (take) => {
+    setDones([...dones, take]);
+    setTaking(taking.filter((item) => item !== take));
+  };
+  // done->takingへ戻す関数
+  const handleBacktoTaking: (done: string) => void = (done) => {
+    setTaking([...taking, done]);
+    setDones(dones.filter((item) => item !== done));
+  };
+  const [isModal, setIsModal] = useState<boolean>(false);
   const handleModalOpen: () => void = () => {
     setIsModal(true);
   };
@@ -15,15 +50,30 @@ const Todo = () => {
         <div className="w-1/3 bg-red-300">
           <h1 className="text-center">Todo</h1>
           <ul>
-            <li>
-              <button>Writting a Note</button>
-            </li>
-            <li>
-              <button>go runnning</button>
-            </li>
-            <li>
-              <button>learning English</button>
-            </li>
+            {todos.map((todo, index) => {
+              return (
+                <li
+                  key={index}
+                  className="flex justify-between pl-2 py-2 hover:bg-slate-200"
+                >
+                  {todo}
+                  <div className="flex pr-2 gap-3">
+                    <button
+                      onClick={() => handleDelete(todo)}
+                      className="flex items-center justify-center hover:text-red-500"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => handleTaking(todo)}
+                      className="flex items-center justify-center hover:text-blue-500"
+                    >
+                      Take
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
           <button
             className="border border-white rounded-md p-1 text-white"
@@ -35,35 +85,60 @@ const Todo = () => {
         <div className="w-1/3 bg-yellow-300">
           <h1 className="text-center">Under Taking</h1>
           <ul>
-            <li>
-              <button>Create Todo List</button>
-            </li>
+            {taking.map((item) => {
+              return (
+                <li className="flex justify-between pl-2 py-2 hover:bg-slate-200">
+                  {item}
+                  <div className="flex pr-2 gap-3">
+                    <button
+                      onClick={() => handleBackToTodos(item)}
+                      className="flex items-center justify-center hover:text-red-500"
+                    >
+                      Undo
+                    </button>
+                    <button
+                      onClick={() => handleDone(item)}
+                      className="flex items-center justify-center hover:text-blue-500"
+                    >
+                      Done
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
-          <div className="flex">
-            <button className="border border-white rounded-md p-1 text-white mr-1">
-              Undo
-            </button>
-            <button className="border border-white rounded-md p-1 text-white">
-              Done
-            </button>
-          </div>
         </div>
         <div className="w-1/3 bg-blue-300">
           <h1 className="text-center">Done</h1>
           <ul>
-            <li>
-              <button>laundry</button>
-            </li>
-            <li>
-              <button>reading</button>
-            </li>
+            {dones.map((done, index) => {
+              return (
+                <li
+                  key={index}
+                  className="flex justify-between pl-2 py-2 hover:bg-slate-200"
+                >
+                  {done}
+                  <div className="flex pr-2 gap-3">
+                    <button
+                      onClick={() => handleBacktoTaking(done)}
+                      className="flex items-center justify-center hover:text-red-500"
+                    >
+                      Undo
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
-          <button className="border border-white rounded-md p-1 text-white">
-            Undo
-          </button>
         </div>
       </div>
-      {isModal && <Modal closeModal={handleModalClose} />}
+      {isModal && (
+        <Modal
+          closeModal={handleModalClose}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      )}
     </div>
   );
 };
